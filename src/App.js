6f9e6fd8c851d4915ae142903a6b52ef5b7fcfdd
prashantchavan <prashantchavan312199@gmail.com>
2022-02-16 22:41:42 +0530

    adding all files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React  from "react";
+import Header from "./component/header";
+import './style.css'
+
+
+function App(){
+    return (
+        <Header/>
+    )
+}
+export default App
---
+++ src/component/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import img from './troll_face.jpeg'
+import '../style.css'
+
+function Header(){
+    return (
+<div className="header">
+    <img  src={img} className="header--img"/>
+   <h2 className="header--titke">Meme Generator</h2>
+   <h4 className="header--project"> React Course - project</h4>
+</div>
+
+    )
+}
+
+export default Header
